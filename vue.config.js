ea8e9be2dfbf0fdeffcbc9966f4b644f01c1a47c
dongwonkim <kkum9408@gmail.com>
2024-02-27 17:46:28 +0900

    composer: set publish option in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,5 @@
-/**
- * md4 algorithm is not available anymore in NodeJS 17+ (because of lib SSL 3).
- * In that case, silently replace md4 by md5 algorithm.
- */
-const crypto = require("crypto");
-try {
-  crypto.createHash('md4');
-} catch (e) {
-  console.warn('Crypto "md4" is not supported anymore by this Node version');
-  const origCreateHash = crypto.createHash;
-  crypto.createHash = (alg, opts) => {
-    return origCreateHash(alg === 'md4' ? 'md5' : alg, opts);
-  };
-}
-
 module.exports = {
+  css: { extract: true },
   chainWebpack: config => {
     config.module
       .rule('ejs')
